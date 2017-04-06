691910cee52af5d069ee4b2e19bb0eaeda135be5
Kim Ki hwang <kimkihwang@Kimui-MacBook-Pro.local>
2017-04-06 18:22:56 +0900

    table practice

---
+++ static/static/MyModel.swift
@@ -0,0 +1,27 @@
+//
+//  MyModel.swift
+//  static
+//
+//  Created by Kim Ki hwang on 2017. 4. 6..
+//  Copyright © 2017년 CodersHigh. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+struct commender {
+    var name:String
+    var trib:String
+}
+
+
+var myModel = [
+    commender(name:"Artanis", trib:"Protos"),
+    commender(name:"Kerrigan", trib:"Zerg"),
+    commender(name: "Raynor", trib:"Terran")
+]
+
